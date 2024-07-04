3e6af4086e96e5ce80f063370f56e41b6cda91ea
mdsiaofficial <mdsiaofficial@gmail.com>
2024-07-04 10:46:55 +0600

    "Updated .gitignore and modified index.js to add new routes and environment variable for port"

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 require('dotenv').config();
 const express = require('express');
 
-// import express from 'express';
-
 const app = express();
-// const port = 3000;
+const port = process.env.PORT || 3000;
 
+
+const githubDATA = {
+  message: 'Github Data',
+  name: "Ashiq",
+  email: "Ashiq",
+  password: "123",
+}
 app.get('/', (req, res) => {
   res.send('Hello');
-})
+});
 
-app.get(`/twitter`, (req, res) => {
-  res.send(`Twitter`);
-})
+app.get('/twitter', (req, res) => {
+  res.send('Twitter');
+});
 
-app.get(`/login`, (req, res) => {
-  res.send(`<h1>Please login</h1>`)
-})
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/login', (req, res) => {
+  res.send('<h1>Please login</h1>');
+});
+
+app.get('/github', (req, res) => {
+  res.json(githubDATA);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
